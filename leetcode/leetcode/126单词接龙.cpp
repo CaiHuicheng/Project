b0a1e685ac6902e1242c39153f60ed691569310a
CaiHuicheng <2751886429@qq.com>
2020-11-19 15:00:05 +0800

    leetCode

--- leetcode/leetcode/126单词接龙.cpp
+++ leetcode/leetcode/126单词接龙.cpp
@@ -1,73 +1,57 @@
-const int INF = 1 << 20;
-
 class Solution {
-private:
-    unordered_map<string, int> wordId;
-    vector<string> idWord;
-    vector<vector<int>> edges;
 public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        int id = 0;
-        for (const string& word : wordList) {
-            if (!wordId.count(word)) {
-                wordId[word] = id++;
-                idWord.push_back(word);
-            }
+    unordered_map<string, int> wordId;
+    vector<vector<int>> edge;
+    int nodeNum = 0;
+
+    void addWord(string& word) {
+        if (!wordId.count(word)) {
+            wordId[word] = nodeNum++;
+            edge.emplace_back();
         }
-        if (!wordId.count(endWord)) {
-            return {};
-        }
-        if (!wordId.count(beginWord)) {
-            wordId[beginWord] = id++;
-            idWord.push_back(beginWord);
-        }
-        edges.resize(idWord.size());
-        for (int i = 0; i < idWord.size(); i++) {
-            for (int j = i + 1; j < idWord.size(); j++) {
-                if (transformCheck(idWord[i], idWord[j])) {
-                    edges[i].push_back(j);
-                    edges[j].push_back(i);
-                }
-            }
-        }
-        const int dest = wordId[endWord];
-        vector<vector<string>> res;
-        queue<vector<int>> q;
-        vector<int> cost(id, INF);
-        q.push(vector<int>{wordId[beginWord]});
-        cost[wordId[beginWord]] = 0;
-        while (!q.empty()) {
-            vector<int> now = q.front();
-            q.pop();
-            int last = now.back();
-            if (last == dest) {
-                vector<string> tmp;
-                for (int index : now) {
-                    tmp.push_back(idWord[index]);
-                }
-                res.push_back(tmp);
-            } else {
-                for (int i = 0; i < edges[last].size(); i++) {
-                    int to = edges[last][i];
-                    if (cost[last] + 1 <= cost[to]) {
-                        cost[to] = cost[last] + 1;
-                        vector<int> tmp(now);
-                        tmp.push_back(to);
-                        q.push(tmp);
-                    }
-                }
-            }
-        }
-        return res;
     }
 
-    bool transformCheck(const string& str1, const string& str2) {
-        int differences = 0;
-        for (int i = 0; i < str1.size() && differences < 2; i++) {
-            if (str1[i] != str2[i]) {
-                ++differences;
+    void addEdge(string& word) {
+        addWord(word);
+        int id1 = wordId[word];
+        for (char& it : word) {
+            char tmp = it;
+            it = '*';
+            addWord(word);
+            int id2 = wordId[word];
+            edge[id1].push_back(id2);
+            edge[id2].push_back(id1);
+            it = tmp;
+        }
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        for (string& word : wordList) {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.count(endWord)) {
+            return 0;
+        }
+        vector<int> dis(nodeNum, INT_MAX);
+        int beginId = wordId[beginWord], endId = wordId[endWord];
+        dis[beginId] = 0;
+
+        queue<int> que;
+        que.push(beginId);
+        while (!que.empty()) {
+            int x = que.front();
+            que.pop();
+            if (x == endId) {
+                return dis[endId] / 2 + 1;
+            }
+            for (int& it : edge[x]) {
+                if (dis[it] == INT_MAX) {
+                    dis[it] = dis[x] + 1;
+                    que.push(it);
+                }
             }
         }
-        return differences == 1;
+        return 0;
     }
 };
---
+++ leetcode/leetcode/126单词接龙_双bfs.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    unordered_map<string, int> wordId;
+    vector<vector<int>> edge;
+    int nodeNum = 0;
+
+    void addWord(string& word) {
+        if (!wordId.count(word)) {
+            wordId[word] = nodeNum++;
+            edge.emplace_back();
+        }
+    }
+
+    void addEdge(string& word) {
+        addWord(word);
+        int id1 = wordId[word];
+        for (char& it : word) {
+            char tmp = it;
+            it = '*';
+            addWord(word);
+            int id2 = wordId[word];
+            edge[id1].push_back(id2);
+            edge[id2].push_back(id1);
+            it = tmp;
+        }
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        for (string& word : wordList) {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.count(endWord)) {
+            return 0;
+        }
+
+        vector<int> disBegin(nodeNum, INT_MAX);
+        int beginId = wordId[beginWord];
+        disBegin[beginId] = 0;
+        queue<int> queBegin;
+        queBegin.push(beginId);
+
+        vector<int> disEnd(nodeNum, INT_MAX);
+        int endId = wordId[endWord];
+        disEnd[endId] = 0;
+        queue<int> queEnd;
+        queEnd.push(endId);
+
+        while (!queBegin.empty() && !queEnd.empty()) {
+            int queBeginSize = queBegin.size();
+            for (int i = 0; i < queBeginSize; ++i) {
+                int nodeBegin = queBegin.front();
+                queBegin.pop();
+                if (disEnd[nodeBegin] != INT_MAX) {
+                    return (disBegin[nodeBegin] + disEnd[nodeBegin]) / 2 + 1;
+                }
+                for (int& it : edge[nodeBegin]) {
+                    if (disBegin[it] == INT_MAX) {
+                        disBegin[it] = disBegin[nodeBegin] + 1;
+                        queBegin.push(it);
+                    }
+                }
+            }
+
+            int queEndSize = queEnd.size();
+            for (int i = 0; i < queEndSize; ++i) {
+                int nodeEnd = queEnd.front();
+                queEnd.pop();
+                if (disBegin[nodeEnd] != INT_MAX) {
+                    return (disBegin[nodeEnd] + disEnd[nodeEnd]) / 2 + 1;
+                }
+                for (int& it : edge[nodeEnd]) {
+                    if (disEnd[it] == INT_MAX) {
+                        disEnd[it] = disEnd[nodeEnd] + 1;
+                        queEnd.push(it);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
