a68d50343f992ca69b42e6fc3646a19c1dc71f8d
CaiHuicheng <2751886429@qq.com>
2020-11-20 20:44:27 +0800

    leetCode 剑52 两个链表的第一个公共节点

---
+++ leetcode/leetcode/剑52两个链表的第一个公共节点.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p = headA;
+        ListNode *q = headB;
+        while(q!=p){
+            p = p!=nullptr?p->next:headB;
+            q = q!=nullptr?q->next:headA;
+        }
+        return p;
+    }
+};
