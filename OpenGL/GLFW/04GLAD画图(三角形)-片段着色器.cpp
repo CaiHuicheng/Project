22a59d92f8d2e5513e4b355b1330e3751b624d5f
CaiHuicheng <2751886429@qq.com>
2020-11-25 21:47:34 +0800

    learning OPENGL GLAD|EBO|IN_OUT|UNIFROM

---
+++ OpenGL/GLFW/04GLAD画图(三角形)-片段着色器.cpp
@@ -0,0 +1,127 @@
+#include<glad\glad.h>
+#include<iostream>
+#include<GLFW\glfw3.h>
+using namespace std;
+
+//编译片段着色器 生成橙色三角
+/*
+	源码：
+		#version 330 core
+		out vec4 FragColor;
+
+		void main()
+		{
+			FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+		} 
+	
+	1、着色器源码存入字符串
+	2、创建着色器对象
+	3、源码字符串赋予着色器对象
+	4、编译着色器
+
+	1、创建着色器对象
+	2、编译好的着色器附加到程序对象上
+	3、链接生成程序
+
+*/
+const char * fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
+//三角形坐标
+float vertices[] = {//z = 0
+	0.0f, 0.5f, 0.0f,	//(x1,y2)
+	0.5f, -0.5f,0.0f,	//(x2,y2)
+	-0.5f, -0.5f,0.0f	//(x3,y3)
+};
+
+void init();
+void VAOSet();
+
+GLFWwindow *window;
+
+int main() {
+
+	init();
+	VAOSet();
+
+	//编译着色器
+	//2、创建着色器对象
+	int fragmentShader=glCreateShader(GL_FRAGMENT_SHADER);
+	//3、源码字符串赋予着色器对象	
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//4、编译着色器
+	glCompileShader(fragmentShader);
+
+	//1、创建着色器对象
+	int shaderProgram = glCreateProgram();
+	//2、编译好的着色器附加到程序对象上
+	glAttachShader(shaderProgram, fragmentShader);
+	//3、链接生成程序
+	glLinkProgram(shaderProgram);
+
+
+	//渲染
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);//使用着色器
+		glDrawArrays(GL_TRIANGLES, 0.3,GL_STATIC_DRAW);//画三角
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	//退出
+	glfwTerminate();
+	return 0;
+}
+
+void VAOSet() {
+	//生成VBO 顶点缓冲对象;VAO管理VBO
+	unsigned int VBO,VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//绑定
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(VAO);
+	//往显卡写值
+	/*
+		GL_STATIC_DRAW ：数据不会或几乎不会改变。
+		GL_DYNAMIC_DRAW：数据会被改变很多。
+		GL_STREAM_DRAW ：数据每次绘制时都会改变。
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//着色器解释 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+void init() {
+	//初始化
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 600, "Set GLAD", nullptr, nullptr);
+
+
+	if (window == nullptr) {
+		cout << "error : window is nullptr" << endl;
+		glfwTerminate();
+		return ;
+	}
+	//设置上下文
+	glfwMakeContextCurrent(window);
+
+	//glad初始化
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "error : gladLoadGLLoader is nullptr" << endl;
+		return ;
+	}
+}
---
+++ OpenGL/GLFW/05GLAD画图(两个三角形)-不用EBO.cpp
@@ -0,0 +1,148 @@
+#include<glad\glad.h>
+#include<iostream>
+#include<GLFW\glfw3.h>
+using namespace std;
+
+
+//三角形坐标
+float vertices[] = {//z = 0
+	-0.5f, -0.5f, 0.0f,	//(x1,y2)
+	0.5f, -0.5f,0.0f,	//(x2,y2)
+	-0.5f, 0.5f,0.0f,	//(x3,y3)
+
+	- 0.5f, 0.5f,0.0f,
+	0.5f, -0.5f,0.0f,
+	0.5f, 0.5f, 0.0f
+};
+
+void init();
+void VAOSet();
+void shadersSet();
+
+
+GLFWwindow *window;
+int shaderProgram;
+
+int main() {
+
+	init();
+	VAOSet();
+	shadersSet();
+	//设置边框模式
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE/*GL_FILL*/);
+	//渲染
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);//使用顶点、片段着色器
+		glDrawArrays(GL_TRIANGLES, 0.6, GL_STATIC_DRAW);//画三角
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	//退出
+	glfwTerminate();
+	return 0;
+}
+
+void shadersSet() {
+	//编译片段着色器 生成橙色三角
+	/*
+	源码：
+	#version 330 core
+	out vec4 FragColor;
+
+	void main()
+	{
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	}
+
+	1、着色器源码存入字符串
+	2、创建着色器对象
+	3、源码字符串赋予着色器对象
+	4、编译着色器
+
+	1、创建着色器对象
+	2、编译好的着色器附加到程序对象上
+	3、链接生成程序
+
+	*/
+	const char * fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+	const char * vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main() {\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n\0";
+
+
+	//编译着色器
+	//2、创建着色器对象
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//3、源码字符串赋予着色器对象	
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//4、编译着色器
+	glCompileShader(vertexShader);
+	glCompileShader(fragmentShader);
+
+	//1、创建着色器对象
+	shaderProgram = glCreateProgram();
+	//2、编译好的着色器附加到程序对象上
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//3、链接生成程序
+	glLinkProgram(shaderProgram);
+}
+
+void VAOSet() {
+	//生成VBO 顶点缓冲对象;VAO管理VBO
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//绑定
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(VAO);
+	//往显卡写值
+	/*
+	GL_STATIC_DRAW ：数据不会或几乎不会改变。
+	GL_DYNAMIC_DRAW：数据会被改变很多。
+	GL_STREAM_DRAW ：数据每次绘制时都会改变。
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//着色器解释 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+void init() {
+	//初始化
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 600, "Set GLAD", nullptr, nullptr);
+
+
+	if (window == nullptr) {
+		cout << "error : window is nullptr" << endl;
+		glfwTerminate();
+		return;
+	}
+	//设置上下文
+	glfwMakeContextCurrent(window);
+
+	//glad初始化
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "error : gladLoadGLLoader is nullptr" << endl;
+		return;
+	}
+}
