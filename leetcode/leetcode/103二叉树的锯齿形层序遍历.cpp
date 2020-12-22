b5304c67de9f4acffac4f1d9ec26515faab8b7c0
CaiHuicheng <2751886429@qq.com>
2020-12-22 16:55:06 +0800

    leetCode 103

---
+++ leetcode/leetcode/103二叉树的锯齿形层序遍历.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) {
+            return ans;
+        }
+        queue<TreeNode*> nodeQueue;
+        nodeQueue.push(root);
+        bool isOrderLeft = true;
+        while (!nodeQueue.empty()) {
+            deque<int> levelList;
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; ++i) {
+                auto node = nodeQueue.front();
+                nodeQueue.pop();
+                if (isOrderLeft) {
+                    levelList.push_back(node->val);
+                } else {
+                    levelList.push_front(node->val);
+                }
+                if (node->left) {
+                    nodeQueue.push(node->left);
+                }
+                if (node->right) {
+                    nodeQueue.push(node->right);
+                }
+            }
+            ans.emplace_back(vector<int>{levelList.begin(), levelList.end()});
+            isOrderLeft = !isOrderLeft;
+        }
+        return ans;
+    }
+};
